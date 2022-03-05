55db07025113e995398655dde61473d107c864d2
EmanuelDms <emanuel.duarte.maia@gmail.com>
2022-03-04 22:27:22 -0300

    feat!: render a test headline

--- src/index.jsx
+++ src/index.jsx
@@ -1,2 +1,7 @@
-import React from "react";
+// ! Todo arquivo que utiliza .jsx necessita do React importado
+import React from 'react';
+import { render } from "react-dom";
 import { App } from './App';
+
+// ? render(o que eu quero renderizar, dentro de qual elemento eu quero renderizar essa informação)
+render(<h1>Test</h1>, document.getElementById('root'))
