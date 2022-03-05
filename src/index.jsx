f46e8200d495809684d2432c3936d0a7124878cc
EmanuelDms <emanuel.duarte.maia@gmail.com>
2022-03-04 22:30:49 -0300

    feat!: using .jsx without react import

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
-// ! Todo arquivo que utiliza .jsx necessita do React importado
-import React from 'react';
+// ! Em versões mais novas do React (a partir da v17) não é mais necessário importar o React em arquivos que utilizem o .jsx
+//// import React from 'react';
 import { render } from "react-dom";
 import { App } from './App';
 
